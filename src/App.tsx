577f73b922266296e1cc36a45fd8a06222337365
Amir <amir-rt@live.com>
2021-09-12 22:47:48 +0200

    feat: added graphql fetch

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import "./App.scss";
 import SideNav from "./SideNav";
 import NavBar from "./NavBar";
 import Settings from "./Settings";
+import { useQuery } from "@apollo/client";
+import { GET_USERS } from ".";
 
 function App() {
+  const { loading, error, data } = useQuery(GET_USERS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log(data);
   return (
     <div className="App-container">
       <NavBar></NavBar>
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+export const GET_USERS = gql`
+  query {
+    profiles {
+      userName
+      firstName
+      lastName
+      bio
+    }
+  }
+`;
+
+client
+  .query({
+    query: GET_USERS,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
